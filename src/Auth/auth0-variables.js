7d9064d5a38b5bb82d71636d4bcb1667000d3581
ElielHdez <eliel.hj@hotmail.com>
2018-06-13 19:12:35 -0500

    first commit

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'mivoto.auth0.com',
+  clientId: 'unOVtAZa2PGW6ZWxfUa11PSl14wdmfUv',
+  // callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://mivoto-admin.surge.sh/callback'
+}
